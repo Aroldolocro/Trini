deeb48c3a973f60feaeeb8d0355e9806845c703a
Aroldolocro <lolosoto321@gmail.comm>
2022-07-04 15:19:40 -0300

    versión 1.0

---
+++ script.js
@@ -0,0 +1,79 @@
+function oral1(){
+    
+    const preguntas1 = ["Potencial de acción, explicar el gráfico y períodos refractarios, en qué parte de la neurona se produce el potencial.", "Funciones de hormonas tiroideas", "Filtración glomerular, todo con barrera, presiones cómo varían, cómo medir el vfg de un paciente (clearance de creatinina o inulina)"]
+
+    const random = Math.floor(Math.random() * preguntas1.length);
+    document.getElementById("shower").innerText=(random, preguntas1[random]);
+}
+
+function oral2(){
+    
+    const preguntas2 = ["Manejo renal de potasio", "Relación V/Q", "Hipotiroidismo primario", "Vías somatosensoriales", "Afasias de broca y wernicke (también ubicación de las áreas)"]
+
+    const random = Math.floor(Math.random() * preguntas2.length);
+    document.getElementById("shower").innerText=(random, preguntas2[random]);
+}
+
+function oral3(){
+    
+    const preguntas3 = ["Ley de Frank Starling, concepto y determinantes de poscarga.", "Fascículo que conecta habla y comprensión del habla, que afasia me genera si lo lesionó y cuales son sus características", "Estructura se lesiona en una corea."]
+
+    const random = Math.floor(Math.random() * preguntas3.length);
+    document.getElementById("shower").innerText=(random, preguntas3[random]);
+}
+
+function oral4(){
+    
+    const preguntas4 = ["Capacidad de oxígeno", "Anemias", "Volumen minuto", "Embarazo: por qué tiene una presión aumentada, Anemia por hemodilución", "Funciones de la albúmina"]
+
+    const random = Math.floor(Math.random() * preguntas4.length);
+    document.getElementById("shower").innerText=(random, preguntas4[random]);
+}
+
+function oral5(){
+    
+    const preguntas5 = ["Funciones de testosterona: por qué el hombre tiene más posibilidades de infarto que la mujer, relación con LDL/HDL, por qué el hombre tiene más gordura en el abdomen", "Sueño, definir cada onda (ella dijo que las alfas no están cuando estamos con los ojos cerrados)", "Glucocorticoides en el intestino como actua, que sintetiza"]
+
+    const random = Math.floor(Math.random() * preguntas5.length);
+    document.getElementById("shower").innerText=(random, preguntas5[random]);
+}
+
+function oral6(){
+    
+    const preguntas6 = ["Reflejo miotático todo bien explicado, con características del huso y etc.", "Coactivación alfa gamma. Me pregunto el lugar en el que están las alfa motoneuronas de los músculos distales (no entendía ni sabía dónde, hasta que me guió y lo que quiso que diga fue sobre el sistema dorsolateral)", "Insulina y glucagón: todo. Cuando hablé sobre las funciones metabólicas me dijo que defina que era gluconeo y etc y también cuál era el principal sustrato de la gluconeo que viene de la lipolisis. Me pregunto sobre otra hormona que haga lipolisis, glucogenogenesis, gluconeo etc y le dije cortisol.", "Hormona que favorece la excreción de sodio: PNA. Acá también me hizo hablar acerca de sraa y ang2"]
+
+    const random = Math.floor(Math.random() * preguntas6.length);
+    document.getElementById("shower").innerText=(random, preguntas6[random]);
+}
+
+function oral7(){
+    
+    const preguntas7 = ["Determinantes del volumen sistólico", "Regulación hormonal del ciclo menstrual"]
+
+    const random = Math.floor(Math.random() * preguntas7.length);
+    document.getElementById("shower").innerText=(random, preguntas7[random]);
+}
+
+function oral8(){
+    
+    const preguntas8 = ["Sinapsis química", "Diferencia entre ach y adrenalina (síntesis y terminación)", "Tono muscular (hable del reflejo miotático)", "Insulina 3 estímulos 3 fx", "De cortisol 3 funciones a nivel del metabolismo", "Si un paciente viene con vómitos cómo está el comp de agua", "Equilibrio ácido-base y electrolitos (en esta pregunta me maree bastante, para mi era acidosis, pero la Dra. me dijo que era alcalosis)", "A raíz del caso anterior me preguntó: Excreción neta de ácido y acidez titulable, valores, formación de nuevo HCO3, vce, porcentaje normal y cómo estaba la orina en este paciente."]
+
+    const random = Math.floor(Math.random() * preguntas8.length);
+    document.getElementById("shower").innerText=(random, preguntas8[random]);
+}
+
+function oral9(){
+    
+    const preguntas9 = ["Potencial de acción y electrónico", "Regulación de la vesícula biliar", "Bilis y absorcion de lipidos"]
+
+    const random = Math.floor(Math.random() * preguntas9.length);
+    document.getElementById("shower").innerText=(random, preguntas9[random]);
+}
+
+function oral10(){
+    
+    const preguntas10 = ["Regulación de la glucemia, como se produce la liberación de la insulina", "Definir potencial de acción, que diga las características", "Qué es el estrés y cuales son sus fases"]
+
+    const random = Math.floor(Math.random() * preguntas10.length);
+    document.getElementById("shower").innerText=(random, preguntas10[random]);
+}
